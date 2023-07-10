6ae4a2ce869d406e37a9da09c8ca76b16bc3a671
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-07-10 12:31:56 +0300

    Create a WebView and set it as content view of the main activity.

--- app/src/main/java/com/mixlr/panos/webviewdemo/MainActivity.kt
+++ app/src/main/java/com/mixlr/panos/webviewdemo/MainActivity.kt
@@ -2,16 +2,21 @@ package com.mixlr.panos.webviewdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.mixlr.panos.webviewdemo.databinding.ActivityMainBinding
+import android.webkit.WebView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var wvDemo: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        binding.wvDemo.loadUrl("https://www.mixlr.com")
+        wvDemo = WebView(this)
+        setContentView(wvDemo)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        wvDemo.loadUrl("https://www.mixlr.com")
     }
 }
