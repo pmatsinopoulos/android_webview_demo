1d861672d898ad2660681cbe6e2014fb643dd5e8
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-07-10 12:26:41 +0300

    Add webview in design and then load URL.

--- app/src/main/java/com/mixlr/panos/webviewdemo/MainActivity.kt
+++ app/src/main/java/com/mixlr/panos/webviewdemo/MainActivity.kt
@@ -2,10 +2,16 @@ package com.mixlr.panos.webviewdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mixlr.panos.webviewdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.wvDemo.loadUrl("https://www.mixlr.com")
     }
 }
